effca9118299b6c4b7756bc93d25f12c1daf8d85
Emanuel <Emanuel@desktop.lan>
2013-10-26 15:47:10 +0100

    Merge origin/master
    Conflicts:
    	SD1314pl001/src/client/Mensage.java

--- SD1314pl001/src/common/Message.java
+++ SD1314pl001/src/common/Message.java
@@ -3,13 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package common;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  *
  * @author Emanuel
  */
-public class Message {
-    
+public class Message implements Serializable {
+
+    private final MessageType type;
+    private final Date timestamp;
+    private final Serializable data;
+
+    public Message(MessageType type, Serializable data) {
+        this.type = type;
+        this.data = data;
+        this.timestamp = new Date();
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public Serializable getData() {
+        return data;
+    }
 }
