7b9ecbfbccaf34063163c6bdaeb2cd016c5cefa5
Emanuel <Emanuel@desktop.lan>
2013-10-26 15:46:39 +0100

    Merge origin/master
    Conflicts:
    	SD1314pl001/src/client/Mensage.java

---
+++ SD1314pl001/src/common/Event.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author Emanuel
+ */
+public class Event implements Serializable {
+
+    private int id;
+    private Date start;
+    private Date end;
+    private String title;
+    private String description;
+    private Date createdAt;
+    private Date modifiedAt;
+
+    public Event(Date start, Date end, String title, String description) {
+        this.start = start;
+        this.end = end;
+        this.title = title;
+        this.description = description;
+        createdAt = new Date();
+        modifiedAt = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Date modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+}
